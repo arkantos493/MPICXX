adafa7f4c8f390d5607d617e4157a00f40d43dbd
Marcel Breyer <breyer.marcel@web.de>
2019-11-15 23:18:43 +0100

    bug fixed in install script

--- .install_gcc_9.2.0.sh
+++ .install_gcc_9.2.0.sh
@@ -10,7 +10,7 @@ else
     echo "  -- unpacking GCC 9.2.0"
     tar xf gcc-9.2.0.tar.xz
     rm gcc-9.2.0.tar.xz
-    cd gcc-9.2.0
+    cd gcc-9.2.0 && ls -a
     echo "  -- installing prerequisites"
    # ./contrib/download_prerequisites
     cd .. && mkdir gcc-9.2.0-objdir && cd gcc-9.2.0-objdir
