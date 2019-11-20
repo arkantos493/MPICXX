d17f6c0c1e08a8dd0cf070a3424a6b99726dc8db
Marcel Breyer <breyer.marcel@web.de>
2019-11-20 20:02:50 +0100

    split test cases for constructor and assignment operations on mpicxx::info objects

--- test/info/info_assignment_test.cpp
+++ test/info/info_assignment_test.cpp
@@ -1,11 +1,11 @@
 /**
- * @file info_construction_test.cpp
+ * @file info_assignment_test.cpp
  * @author Marcel Breyer
  * @date 2019-11-20
  *
  * @brief Test cases for the @ref mpicxx::info implementation.
  *
- * This file provides test cases for the constructoion of a mpicxx::info object.
+ * This file provides test cases for the assignment operator of a mpicxx::info object.
  */
 
 #include <gtest/gtest.h>
@@ -13,23 +13,10 @@
 
 #include <mpicxx/info/info.hpp>
 
-TEST(InfoTests, DefaultConstruction) {
+TEST(InfoTests, CopyAssignment) {
 
     // default construct a mpicxx::info object
-    mpicxx::info info;
-
-    int nkeys;
-    MPI_Info_get_nkeys(info.get(), &nkeys);
-
-    // a newly created MPI_Info object should be emtpy
-    EXPECT_EQ(nkeys, 0);
-
-}
-
-TEST(InfoTests, CopyConstruction) {
-
-    // default construct a mpicxx::info object
-    mpicxx::info info;
+    mpicxx::info info, info_copy;
 
     // add an element to the info object
     MPI_Info_set(info.get(), "key", "value");
@@ -39,8 +26,8 @@ TEST(InfoTests, CopyConstruction) {
     MPI_Info_get_nkeys(info.get(), &nkeys);
     EXPECT_EQ(nkeys, 1);
 
-    // create an new info object from a copy
-    mpicxx::info info_copy(info);
+    // copy "info" to "info_copy"
+    info_copy = info;
 
     // be sure that the copied key and value are present
     int flag_copy;
