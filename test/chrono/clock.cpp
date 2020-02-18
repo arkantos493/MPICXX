0d1e7c8fb6cfa2c0a19a31929e8c1574f89b5285
Marcel Breyer <breyer.marcel@web.de>
2020-02-18 20:44:21 +0100

    added example, tests and documentation for the mpicxx::clock class

--- test/chrono/clock.cpp
+++ test/chrono/clock.cpp
@@ -1,31 +1,44 @@
 /**
  * @file test/chrono/clock.cpp
  * @author Marcel Breyer
- * @date 2020-02-17
+ * @date 2020-02-18
  *
  * @brief Test cases for the @ref mpicxx::clock class.
  * @details Testsuite: *ClockTest*
- * | test case name               | test case description                   |
- * |:-----------------------------|:----------------------------------------|
- * |                              |                                         |
+ * | test case name | test case description                      |
+ * |:---------------|:-------------------------------------------|
+ * | Now            | check the static `now()` function          |
+ * | Resolution     | check the static `resolution()` function   |
+ * | Synchronized   | check the static `synchronized()` function |
  */
+
+#include <chrono>
+#include <thread>
 
 #include <gtest/gtest.h>
 
 #include <mpicxx/chrono/clock.hpp>
 
-#include <iostream>
-#include <iomanip>
-#include <chrono>
-#include <thread>
-TEST(ClockTest, ClockTest) {
 
-    std::cout << "Tick Rate: " << mpicxx::clock::resolution() << std::endl;
-
+TEST(ClockTest, Now) {
+    // get current wall-clock time
     auto start = mpicxx::clock::now();
-    std::chrono::milliseconds timespan(5050); // or whatever
+
+    // wait for 1s
+    std::chrono::milliseconds timespan(1000);
     std::this_thread::sleep_for(timespan);
+
+    // get current wall-clock time
     auto end = mpicxx::clock::now();
 
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+    // the duration in ms should be at least 1000
+    EXPECT_GE(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(), 1000);
+}
+
+TEST(ClockTest, Resolution) {
+    EXPECT_LE(mpicxx::clock::resolution(), 1.0);
+}
+
+TEST(ClockTest, Synchronized) {
+    EXPECT_FALSE(mpicxx::clock::synchronized());
 }
