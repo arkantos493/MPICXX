69ce251c75155929a606f2e2264a722f90c62f36
Marcel Breyer <breyer.marcel@web.de>
2020-02-18 20:55:45 +0100

    changed .gitignore

---
+++ include/mpicxx/config.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file include/mpicxx/config.hpp
+ * @author Marcel Breyer
+ * @date 2020-02-08
+ *
+ * @brief Defines version macros for the mpicxx library.
+ * @details This macros are:
+ * - `PROJECT_NAME`: mpicxx
+ * - `PROJECT_VERSION`: "MAJOR.MINOR.PATCH"
+ * - `PROJECT_VERSION_MAJOR`
+ * - `PROJECT_VERSION_MINOR`
+ * - `PROJECT_VERSION_PATCH`
+ */
+
+#ifndef MPICXX_CONFIG_HPP
+#define MPICXX_CONFIG_HPP
+
+#define PROJECT_NAME "mpicxx"
+#define PROJECT_VERSION  "0.2.0"
+#define PROJECT_VERSION_MAJOR "0"
+#define PROJECT_VERSION_MINOR "2"
+#define PROJECT_VERSION_PATCH "0"
+
+#endif // MPICXX_CONFIG_HPP
