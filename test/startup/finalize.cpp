7740212dd2ff8b7ed83eb6d8ec307d247dd27691
Marcel Breyer <breyer.marcel@web.de>
2020-02-20 21:45:17 +0100

    created test cases for the new functions

---
+++ test/startup/finalize.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file test/startup/finalize.cpp
+ * @author Marcel Breyer
+ * @date 2020-02-20
+ *
+ * @brief Test cases for the @ref mpicxx::finalized() function.
+ * @details Testsuite: *StartupTest*
+ * | test case name | test case description                              |
+ * |:---------------|:---------------------------------------------------|
+ * | IsFinalized    | check that no *MPI_Finalize()* has been called yet |
+ */
+
+#include <gtest/gtest.h>
+
+#include <mpicxx/startup/finalization.hpp>
+
+
+TEST(StartupTest, IsFinalized) {
+    // MPI shouldn't be finalized yet
+    EXPECT_FALSE(mpicxx::finalized());
+}
