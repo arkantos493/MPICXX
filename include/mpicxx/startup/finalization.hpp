e582b35541edae06e4eb49b99e112ef40a80b97d
Marcel Breyer <breyer.marcel@web.de>
2020-02-20 21:46:34 +0100

    documented finalization functions

--- include/mpicxx/startup/finalization.hpp
+++ include/mpicxx/startup/finalization.hpp
@@ -1,7 +1,7 @@
 /**
  * @file include/mpicxx/startup/finalization.hpp
  * @author Marcel Breyer
- * @date 2020-02-19
+ * @date 2020-02-20
  *
  * @brief Implements wrapper around the MPI finalization functions.
  */
@@ -14,10 +14,25 @@
 
 namespace mpicxx {
 
+    /**
+     * @brief Clean up die MPI state.
+     * @details If an MPI program terminates normally (i.e., not due to a call to @ref mpicxx::abort() or an unrecoverable error) then each
+     * process  must call @ref mpicxx::finalize() before it exits. Before an MPI process invokes @ref mpicxx::finalize(), the process must
+     * perform all MPI calls needed to complete its involvement in MPI communications.
+     *
+     * Once @ref mpicxx::finalize() returns, no MPI routine (not even @ref mpicxx::initialize()) may be called, except for
+     * @ref mpicxx::version(), @ref mpicxx::library_version(), @ref mpicxx::initialized(), @ref mpicxx::finalized(), and any MPI Tool
+     * function.
+     */
     inline void finalize() {
         MPI_Finalize();
     }
 
+    /**
+     * @brief Checks whether @ref mpicxx::finalize() has completed.
+     * @details It is valid to call @ref mpicxx::finalized() before @ref mpicxx::initialize() and after @ref mpicxx::finalize().
+     * @return `true` if @ref mpicxx::finalize() has completed, otherwise `false`
+     */
     [[nodiscard]] inline bool finalized() {
         int flag;
         MPI_Finalized(&flag);
