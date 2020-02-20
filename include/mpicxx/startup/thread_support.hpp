4327782ba4d5ce7d5779c28ba7618ee9a3f0023c
Marcel Breyer <breyer.marcel@web.de>
2020-02-20 21:46:56 +0100

    documented thread_support enumeration and fmt formatter

--- include/mpicxx/startup/thread_support.hpp
+++ include/mpicxx/startup/thread_support.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file include/mpicxx/startup/thread_support.hpp
+ * @author Marcel Breyer
+ * @date 2020-02-20
+ *
+ * @brief Contains the level of thread support enum.
+ * @details Additionally specializes a [*fmt*](https://github.com/fmtlib/fmt) formatter to print the actual thread support names.
+ */
+
 #ifndef MPICXX_THREAD_SUPPORT_HPP
 #define MPICXX_THREAD_SUPPORT_HPP
 
@@ -9,15 +18,26 @@
 
 namespace mpicxx {
 
-    enum class thread_support {
+    /**
+     * @brief Enum class for the different levels of thread support provided by MPI.
+     */
+    enum thread_support {
+        /** only one thread will execute */
         SINGLE = MPI_THREAD_SINGLE,
+        /** the process may be multi-threaded, but the application must ensure that only the main thread makes MPI calls */
         FUNNELED = MPI_THREAD_FUNNELED,
+        /** the process may be multi-threaded, and multiple threads may make MPI calls, but only one at a time */
         SERIALIZED = MPI_THREAD_SERIALIZED,
-        MULTIPLE = MPI_THREAD_MULTIPLE
+        /** multiple threads may call MPI, with no restrictions */
+        MULTIPLE = MPI_THREAD_MULTIPLE,
     };
 
 }
 
+// TODO 2020-02-20 21:18 marcel: change from fmt::format to std::format
+/**
+ * @brief Custom [*fmt*](https://github.com/fmtlib/fmt) formatter for the mpicxx::thread_support enum class.
+ */
 template <>
 struct fmt::formatter<mpicxx::thread_support> : fmt::formatter<string_view> {
     template <typename FormatContext>
