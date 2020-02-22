aa11da31091a5fd117c93dfe32c9612ac0fab7ec
Marcel Breyer <breyer.marcel@web.de>
2020-02-22 21:38:43 +0100

    added documentation

--- include/mpicxx/startup/mpicxx_main.hpp
+++ include/mpicxx/startup/mpicxx_main.hpp
@@ -1,9 +1,19 @@
+/**
+ * @file include/mpicxx/startup/mpicxx_main.hpp
+ * @author Marcel Breyer
+ * @date 2020-02-22
+ *
+ * @brief Implements a save way to setup and teardown the MPI environment, e.g. without the possibility to forget a call to *MPI_Init* or
+ * *MPI_Finalize*.
+ */
+
+
 #ifndef MPICXX_MPICXX_MAIN_HPP
 #define MPICXX_MPICXX_MAIN_HPP
 
-#include <mpicxx/startup/initialization.hpp>
-#include <mpicxx/startup/finalization.hpp>
 #include <mpicxx/detail/concepts.hpp>
+#include <mpicxx/startup/finalization.hpp>
+#include <mpicxx/startup/initialization.hpp>
 
 #include <functional>
 #include <iostream>
@@ -11,6 +21,45 @@
 
 namespace mpicxx {
 
+    /**
+     * @brief Correctly setup and teardown the MPI environment while executing the code given by @p ptr.
+     * @details This function performs the following tasks in the given order:
+     * 1. call @ref mpicxx::initialize()
+     * 2. invoke the function represented by @p ptr
+     * 3. call @ref mpicxx::finalize()
+     *
+     * Example:
+     * @code{.cpp}
+     * int main() {
+     *     mpicxx::initialize();  // don't forget the initialization call (exactly once)!
+     *     {                      // brackets needed or mpicxx objects would get destroyed after the finalization call which is not permitted
+     *
+     *         // user code
+     *
+     *     }
+     *     mpicxx::finalize();    // don't forget the finalization call (exactly once)!
+     *     return 0;
+     * }
+     * @endcode
+     * is the same as:
+     * @code{.cpp}
+     * int mpicxx_main() {
+     *     // no need to call a initialization function!
+     *
+     *     // user code
+     *
+     *     // no need to call a finalization function!
+     *     return 0;
+     * }
+     *
+     * int main() {
+     *     return mpicxx::init(&mpicxx_main);   // can't make any mistake related to initialization or finalization!
+     * }
+     * @endcode
+     * @tparam FuncPtr a callable fulfilling the @ref detail::main_pointer requirements
+     * @param[in] ptr any callable holding the main code of the application
+     * @return the result of the invocation of @p FuncPtr
+     */
     template <detail::main_pointer FuncPtr>
     int init(FuncPtr ptr) {
         initialize();
@@ -20,9 +69,49 @@ namespace mpicxx {
         finalize();
         return ret;
     }
-
+    /**
+     * @brief Correctly setup and teardown the MPI environment while executing the code given by @p ptr.
+     * @details This function performs the following tasks in the given order:
+     * 1. call @ref mpicxx::initialize(int& argc, char** argv)
+     * 2. invoke the function represented by @p ptr
+     * 3. call @ref mpicxx::finalize()
+     *
+     * Example:
+     * @code{.cpp}
+     * int main(int argc, char** argv) {
+     *     mpicxx::initialize(argc, argv);  // don't forget the initialization call (exactly once)!
+     *     {                                // brackets needed or mpicxx objects would get destroyed after the finalization call which is not permitted
+     *
+     *         // user code
+     *
+     *     }
+     *     mpicxx::finalize();              // don't forget the finalization call (exactly once)!
+     *     return 0;
+     * }
+     * @endcode
+     * is the same as:
+     * @code{.cpp}
+     * int mpicxx_main(int argc, char** argv) {
+     *     // no need to call a initialization function!
+     *
+     *     // user code
+     *
+     *     // no need to call a finalization function!
+     *     return 0;
+     * }
+     *
+     * int main(int argc, char** argv) {
+     *     return mpicxx::init(&mpicxx_main, argc, argv);   // can't make any mistake related to initialization or finalization!
+     * }
+     * @endcode
+     * @tparam FuncPtr a callable fulfilling the @ref detail::main_args_pointer requirements
+     * @param[in] ptr any callable holding the main code of the application
+     * @param[inout] argc the number of command line parameters
+     * @param[inout] argv the command line parameters
+     * @return the result of the invocation of @p FuncPtr or -1 if the required level level of thread support couldn't be satisfied
+     */
     template <detail::main_args_pointer FuncPtr>
-    int init(FuncPtr ptr, int& argc, char** argv) {
+    int init(FuncPtr ptr, int& argc, char** argv) { // TODO 2020-02-22 21:17 marcel: argc as int or int&
         initialize(argc, argv);
 
         int ret = std::invoke(ptr, argc, argv);
@@ -30,12 +119,55 @@ namespace mpicxx {
         finalize();
         return ret;
     }
-
+    /**
+     * @brief Correctly setup and teardown the MPI environment with the required level of thread support while executing the code given by
+     * @p ptr. If the required level of thread support couldn't be satisfied, the function returns immediately with return code -1.
+     * @details This function performs the following tasks in the given order:
+     * 1. call @ref mpicxx::initialize(const thread_support)
+     * 2. invoke the function represented by @p ptr
+     * 3. call @ref mpicxx::finalize()
+     *
+     * Example:
+     * @code{.cpp}
+     * int main() {
+     *     try {
+     *         mpicxx::initialize(mpicxx::thread_support::MULTIPLE);  // don't forget the initialization call (exactly once)!
+     *
+     *         // user code
+     *
+     *     } catch(const mpicxx::thread_support_not_satisfied& e) {
+     *         std::cout << e.what() << std::endl;
+     *     }
+     *     mpicxx::finalize();                                        // don't forget the finalization call even in case of an exception (exactly once)!
+     *     return 0;
+     * }
+     * @endcode
+     * is nearly the same as (except for the return value):
+     * @code{.cpp}
+     * int mpicxx_main() {
+     *     // no need to call a initialization function!
+     *     // no need to catch an exception; if this function gets called, the required level of thread support could be satisfied
+     *
+     *     // user code
+     *
+     *     // no need to call a finalization function!
+     *     return 0;
+     * }
+     *
+     * int main() {
+     *     return mpicxx::init(&mpicxx_main, mpicxx::thread_support::MULTIPLE);   // can't make any mistake related to initialization or finalization!
+     * }
+     * @endcode
+     * @tparam FuncPtr a callable fulfilling the @ref detail::main_pointer requirements
+     * @param[in] ptr any callable holding the main code of the application
+     * @param[in] required the required level of thread support
+     * @return the result of the invocation of @p FuncPtr
+     */
     template <detail::main_pointer FuncPtr>
-    int init(FuncPtr ptr, const thread_support ts) {
+    int init(FuncPtr ptr, const thread_support required) {
         int ret = -1;
         try {
-            initialize(ts);
+            initialize(required);
             ret = std::invoke(ptr);
         } catch (const mpicxx::thread_support_not_satisfied& e) {
             std::cerr << e.what() << std::endl;
@@ -44,12 +176,57 @@ namespace mpicxx {
         finalize();
         return ret;
     }
-
+    /**
+     * @brief Correctly setup and teardown the MPI environment with the required level of thread support while executing the code given by
+     * @p ptr. If the required level of thread support couldn't be satisfied, the function returns immediately with return code -1.
+     * @details This function performs the following tasks in the given order:
+     * 1. call @ref mpicxx::initialize(const thread_support)
+     * 2. invoke the function represented by @p ptr
+     * 3. call @ref mpicxx::finalize()
+     *
+     * Example:
+     * @code{.cpp}
+     * int main(int argc, char** argv) {
+     *     try {
+     *         mpicxx::initialize(argc, argv, mpicxx::thread_support::MULTIPLE);  // don't forget the initialization call (exactly once)!
+     *
+     *         // user code
+     *
+     *     } catch(const mpicxx::thread_support_not_satisfied& e) {
+     *         std::cout << e.what() << std::endl;
+     *     }
+     *     mpicxx::finalize();                                                    // don't forget the finalization call even in case of an exception (exactly once)!
+     *     return 0;
+     * }
+     * @endcode
+     * is nearly the same as (except for the return value):
+     * @code{.cpp}
+     * int mpicxx_main(int argc, char** argv) {
+     *     // no need to call a initialization function!
+     *     // no need to catch an exception; if this function gets called, the required level of thread support could be satisfied
+     *
+     *     // user code
+     *
+     *     // no need to call a finalization function!
+     *     return 0;
+     * }
+     *
+     * int main(int argc, char** argv) {
+     *     return mpicxx::init(&mpicxx_main, argc, argv, mpicxx::thread_support::MULTIPLE);   // can't make any mistake related to initialization or finalization!
+     * }
+     * @endcode
+     * @tparam FuncPtr a callable fulfilling the @ref detail::main_pointer requirements
+     * @param[in] ptr any callable holding the main code of the application
+     * @param[inout] argc the number of command line parameters
+     * @param[inout] argv the command line parameters
+     * @param[in] required the required level of thread support
+     * @return the result of the invocation of @p FuncPtr or -1 if the required level level of thread support couldn't be satisfied
+     */
     template <detail::main_args_pointer FuncPtr>
-    int init(FuncPtr ptr, int& argc, char** argv, const thread_support ts) {
+    int init(FuncPtr ptr, int& argc, char** argv, const thread_support required) {
         int ret = -1;
         try {
-            initialize(argc, argv, ts);
+            initialize(argc, argv, required);
             ret = std::invoke(ptr, argc, argv);
         } catch (const mpicxx::thread_support_not_satisfied& e) {
             std::cerr << e.what() << std::endl;
