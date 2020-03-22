fa8ca09a7e1455a92014c7e4def28753b8e93af2
Marcel Breyer <breyer.marcel@web.de>
2020-03-22 20:10:22 +0100

    added documentation to the conversion functions

--- include/mpicxx/detail/conversion.hpp
+++ include/mpicxx/detail/conversion.hpp
@@ -1,7 +1,7 @@
 /**
  * @file include/mpicxx/detail/conversion.hpp
  * @author Marcel Breyer
- * @date 2020-03-19
+ * @date 2020-03-22
  *
  * @brief Defines conversion functions used in the mpicxx library.
  */
@@ -16,13 +16,30 @@
 
 namespace mpicxx::detail {
 
+    /**
+     * @brief Converts a boolean value to its string representation.
+     * @param val the boolean value
+     * @return `std::string("true")` or `std::string("false")`
+     */
     std::string convert_to_string(const bool val) {
         using namespace std::string_literals;
         return val ? "true"s : "false"s;
     }
+    /**
+     * @brief Converts every *string* like type to a [`std::string`](https://en.cppreference.com/w/cpp/string/basic_string).
+     * @param val a *string* like value
+     * @return a [`std::string`](https://en.cppreference.com/w/cpp/string/basic_string) with the same content as @p val
+     */
     std::string convert_to_string(string auto&& val) {
         return std::string(std::forward<decltype(val)>(val));
     }
+    /**
+     * @brief Converts the given @p val to its string representation using the
+     * [`to_string()](https://en.cppreference.com/w/cpp/string/basic_string/to_string) function.
+     * @tparam T the convert to type
+     * @param val the value to convert
+     * @return the string representation of @p val
+     */
     template <typename T>
     std::string convert_to_string(T&& val) {
         using std::to_string;
