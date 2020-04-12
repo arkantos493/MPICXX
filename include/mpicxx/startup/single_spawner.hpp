60fc6564c7831d9649922b9139a3bee7a33ee80a
Marcel Breyer <b-marci@web.de>
2020-04-12 20:35:03 +0200

    improved/added documentation, added assertion

--- include/mpicxx/startup/single_spawner.hpp
+++ include/mpicxx/startup/single_spawner.hpp
@@ -1,15 +1,16 @@
 /**
- * @file include/mpicxx/startup/spawner.hpp
+ * @file include/mpicxx/startup/single_spawner.hpp
  * @author Marcel Breyer
- * @date 2020-04-09
+ * @date 2020-04-12
  *
  * @brief Implements wrapper around the *MPI_COMM_SPAWN* function.
  */
 
-#ifndef MPICXX_SPAWNER_HPP
-#define MPICXX_SPAWNER_HPP
+#ifndef MPICXX_SINGLE_SPAWNER_HPP
+#define MPICXX_SINGLE_SPAWNER_HPP
 
 #include <iostream>
+#include <iterator>
 #include <numeric>
 #include <stdexcept>
 #include <string>
@@ -30,19 +31,24 @@
 namespace mpicxx {
 
     // TODO 2020-03-22 19:04 marcel: change from MPI_Comm to mpicxx equivalent
-    // TODO 2020-03-23 12:56 marcel: errcode equivalent
+    // TODO 2020-03-23 12:56 marcel: change from int to mpicxx errcode equivalent
     // TODO 2020-03-23 12:56 marcel: change from fmt::format to std::format
     // TODO 2020-03-23 17:37 marcel: copy/move constructor/assignment
 
     /**
-     * @brief Spawner class which enables to spawn MPI processes at runtime.
+     * @brief Spawner class which enables to spawn (multiple) MPI processes at runtime.
      */
     class single_spawner {
-        /// the type of a single argv argument (including a key and a value)
+        /// The type of a single argv argument (represented by  a key and value).
         using argv_type = std::pair<std::string, std::string>;
     public:
+        // ---------------------------------------------------------------------------------------------------------- //
+        //                                               constructor                                                  //
+        // ---------------------------------------------------------------------------------------------------------- //
+        /// @name constructor
+        ///@{
         /**
-         * @brief Create a new single_spawner.
+         * @brief Construct a new single_spawner object.
          * @param[in] command name of program to be spawned
          * @param[in] maxprocs maximum number of processes to start
          *
@@ -60,27 +66,24 @@ namespace mpicxx {
         {
             MPICXX_ASSERT_SANITY(!command_.empty(), "No executable name given!");
         }
+        ///@}
 
 
         // ---------------------------------------------------------------------------------------------------------- //
-        //                                                  getter                                                    //
+        //                                       getter/setter spawn information                                      //
         // ---------------------------------------------------------------------------------------------------------- //
+        /// @name PLACEHOLDER
+        ///@{
         /**
          * @brief Returns the name of the executable which should get spawned.
-         * @return the executable name
+         * @return the executable name (`[[nodiscard]]`)
          */
         [[nodiscard]] const std::string& command() const noexcept { return command_; }
-
         /**
          * @brief Returns the number of processes which should get spawned.
-         * @return the number of processes
+         * @return the number of processes (`[[nodiscard]]`)
          */
         [[nodiscard]] int maxprocs() const noexcept { return maxprocs_; }
-
-
-        // ---------------------------------------------------------------------------------------------------------- //
-        //                                         manipulate additional info                                         //
-        // ---------------------------------------------------------------------------------------------------------- //
         /**
          * @brief Set the info object representing additional information for the runtime system where and how to spawn the processes.
          * @details As of [MPI standard 3.1](https://www.mpi-forum.org/docs/mpi-3.1/mpi31-report.pdf) reserved keys are:
@@ -97,22 +100,18 @@ namespace mpicxx {
          * @note An implementation is not required to interpret these keys, but if it does interpret the key, it must provide the
          * functionality described.
          * @param[in] additional_info copy of the info object
+         *
+         * @attention The user **has to ensure** that @p additional_info doesn't go out-of-scope before @ref spawn() gets called.
          */
-        single_spawner& set_spawn_info(info additional_info) noexcept(std::is_nothrow_move_assignable_v<info>) {
-            // TODO 2020-03-22 18:39 marcel: asserts?, parameter type?, calls?
-            info_ = std::move(additional_info);
+        single_spawner& set_spawn_info(const info& additional_info) noexcept {
+            info_ = &additional_info;
             return *this;
         }
         /**
          * @brief Returns the info object representing additional information for the runtime system where and how to spawn the processes.
-         * @return the info object
+         * @return the info object (`[[nodiscard]]`)
          */
-        [[nodiscard]] const info& spawn_info() const noexcept { return info_; }
-
-
-        // ---------------------------------------------------------------------------------------------------------- //
-        //                                       manipulate additional arguments                                      //
-        // ---------------------------------------------------------------------------------------------------------- //
+        [[nodiscard]] const info& spawn_info() const noexcept { return *info_; }
         /**
          * @brief Adds an argument pair the the `argv` list which gets passed to the spawned program.
          * @details Adds a leading `-` to @p key if not already present.
@@ -132,28 +131,46 @@ namespace mpicxx {
             argv_.emplace_back(std::move(key), detail::convert_to_string(std::forward<T>(value)));
             return *this;
         }
+        /**
+         * @brief Adds all elements from the range [@p first, @p last)  to the `argvs` list.
+         * @tparam InputIt must meet the requirements of [LegacyInputIterator](https://en.cppreference.com/w/cpp/named_req/InputIterator).
+         * @param[in] first iterator to the first `argv` in the range
+         * @param[in] last iterator one-past the last `argv` in the range
+         *
+         * @pre @p first and @p last **must** refer to the same container.
+         * @pre @p first and @p last **must** form a valid range, i.e. @p first must be less or equal than @p last.
+         *
+         * @assert_precondition{ If @p first and @p last don't denote a valid range. }
+         */
         template <std::input_iterator InputIt>
-        requires (!std::is_constructible_v<std::string, InputIt>) // TODO 2020-04-09 18:52 marcel: revise
-        single_spawner& add_argv(InputIt first, InputIt last) {
+        single_spawner& add_argv(InputIt first, InputIt last) requires (!std::is_constructible_v<std::string, InputIt>) {
+            MPICXX_ASSERT_PRECONDITION(this->legal_iterator_range(first, last),
+                    "Attempt to pass an illegal iterator range ('first' must be less or equal than 'last')!");
+
             for (; first != last; ++first) {
                 auto&& pair = *first;
                 this->add_argv(std::forward<decltype(pair.first)>(pair.first), std::forward<decltype(pair.second)>(pair.second));
             }
             return *this;
         }
+        /**
+         * @brief Adds all elements from the initializer list @p ilist to the `argvs` list.
+         * @tparam T the type of the value
+         * @param[in] ilist initializer list to insert the `argvs` from
+         */
         template <typename T>
         single_spawner& add_argv(std::initializer_list<std::pair<std::string, T>> ilist) {
             return this->add_argv(ilist.begin(), ilist.end());
         }
         /**
          * @brief Returns the arguments which will be passed to `command`.
-         * @return the arguments passed to `command`
+         * @return the arguments passed to `command` (`[[nodiscard]]`)
          */
         [[nodiscard]] const std::vector<argv_type>& argv() const noexcept { return argv_; }
         /**
          * @brief Returns the i-th argument which will be passed to `command`.
          * @param[in] i the argv to return
-         * @return the i-th argument
+         * @return the i-th argument (`[[nodiscard]]`)
          *
          * @throws std::out_of_range if the index @p i is an out-of-bounce access
          */
@@ -164,6 +181,7 @@ namespace mpicxx {
 
             return argv_[i];
         }
+        ///@}
 
 
         // ---------------------------------------------------------------------------------------------------------- //
@@ -172,14 +190,20 @@ namespace mpicxx {
         /**
          * @brief Spawns a number of MPI processes according to the previously set options.
          *
+         * @pre The set info object **should not** be out-of-scope.
+         *
+         * @assert_precondition{ If the info object is out-of-scope. }
+         *
          * @calls{
          * int MPI_Comm_spawn(const char *command, char *argv[], int maxprocs, MPI_Info info, int root, MPI_Comm comm, MPI_Comm *intercomm, int array_of_errcodes[]);       // exactly once
          * }
          */
         void spawn() {
+            MPICXX_ASSERT_PRECONDITION(info_ != nullptr, "The spawn info object went out-of-scope!");
+
             if (argv_.empty()) {
                 // no additional arguments provided -> use MPI_ARGV_NULL
-                MPI_Comm_spawn(command_.c_str(), MPI_ARGV_NULL, maxprocs_, info_.get(),
+                MPI_Comm_spawn(command_.c_str(), MPI_ARGV_NULL, maxprocs_, info_->get(),
                                base_.root_, base_.comm_, &base_.intercomm_, base_.errcodes_.data());
             } else {
                 // convert additional arguments to char**
@@ -192,7 +216,7 @@ namespace mpicxx {
                 // add null termination
                 argv_ptr.emplace_back(nullptr);
 
-                MPI_Comm_spawn(command_.c_str(), argv_ptr.data(), maxprocs_, info_.get(),
+                MPI_Comm_spawn(command_.c_str(), argv_ptr.data(), maxprocs_, info_->get(),
                                base_.root_, base_.comm_, &base_.intercomm_, base_.errcodes_.data());
             }
         }
@@ -260,12 +284,22 @@ namespace mpicxx {
 
 
     private:
+#if ASSERTION_LEVEL > 0
+        /*
+         * @brief Check whether @p first and @p last denote a valid range, i.e. @p first is less or equal than @p last.
+         * @details Checks whether the distance bewteen @p first and @p last is not negative.
+         */
+        template <std::input_iterator InputIt>
+        bool legal_iterator_range(InputIt first, InputIt last) {
+            return std::distance(first, last) >= 0;
+        }
+#endif
         detail::spawner_base base_;
 
         std::string command_;
         int maxprocs_;
         std::vector<argv_type> argv_;
-        info info_ = info(MPI_INFO_NULL, false);
+        const info* info_ = &mpicxx::info::null;
     };
 
     /// default spawner is a @ref single_spawner
@@ -274,4 +308,4 @@ namespace mpicxx {
 }
 
 
-#endif // MPICXX_SPAWNER_HPP
+#endif // MPICXX_SINGLE_SPAWNER_HPP
