ed75805917df4a6b5c932902e532ef313f994acb
Marcel Breyer <b-marci@web.de>
2020-06-17 00:49:32 +0200

    changed [[nodiscard]] formatting

--- include/mpicxx/info/runtime_info.hpp
+++ include/mpicxx/info/runtime_info.hpp
@@ -1,7 +1,7 @@
 /**
  * @file include/mpicxx/info/runtime_info.hpp
  * @author Marcel Breyer
- * @date 2020-05-18
+ * @date 2020-06-17
  *
  * @brief Implements functions to query various information during runtime.
  */
@@ -10,26 +10,24 @@
 #define MPICXX_RUNTIME_INFO_HPP
 
 #include <optional>
+#include <string>
 
 #include <mpi.h>
 
 
-// TODO 2020-05-18 01:35 breyerml: better name
 namespace mpicxx {
 
-    // TODO 2020-05-18 01:49 breyerml: add doxygen section
     /**
      * @brief Returns the maximum possible number of processes.
-     * @return an optional containing the maximum possible number of processes or
-     * [`std::nullopt`](https://en.cppreference.com/w/cpp/utility/optional/nullopt) if no value could be retrieved (`[[nodiscard]]`)
+     * @return a [`std::optional`](https://en.cppreference.com/w/cpp/utility/optional) containing the maximum possible number of processes or
+     *         [`std::nullopt`](https://en.cppreference.com/w/cpp/utility/optional/nullopt) if no value could be retrieved (`[[nodiscard]]`)
      *
      * @note It may be possible that less than `universe_size` processes can be spawned if processes are already running.
      *
-     * @calls{
-     * int MPI_Comm_get_attr(MPI_Comm comm, int comm_keyval, void *attribute_val, int *flag);       // exactly once
-     * }
+     * @calls{ int MPI_Comm_get_attr(MPI_Comm comm, int comm_keyval, void *attribute_val, int *flag);       // exactly once }
      */
-    [[nodiscard]] inline std::optional<int> universe_size() {
+    [[nodiscard]]
+    inline std::optional<int> universe_size() {
         void* ptr;
         int flag;
         MPI_Comm_get_attr(MPI_COMM_WORLD, MPI_UNIVERSE_SIZE, &ptr, &flag);
@@ -44,11 +42,10 @@ namespace mpicxx {
      * @brief Returns the name of the processor this code is currently running on.
      * @return the name of the processor this code is running on (`[[nodiscard]]`)
      *
-     * @calls{
-     * int MPI_Get_processor_name(char *name, int *resultlen);      // exactly once
-     * }
+     * @calls{ int MPI_Get_processor_name(char *name, int *resultlen);      // exactly once }
      */
-    [[nodiscard]] inline std::string processor_name() {
+    [[nodiscard]]
+    inline std::string processor_name() {
         char name[MPI_MAX_PROCESSOR_NAME];
         int resultlen;
         MPI_Get_processor_name(name, &resultlen);
