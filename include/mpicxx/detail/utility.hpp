1b5a2e10d5295b3c792931e2e88769daa8924f47
Marcel Breyer <b-marci@web.de>
2020-06-23 00:21:02 +0200

    added missing documentation

--- include/mpicxx/detail/utility.hpp
+++ include/mpicxx/detail/utility.hpp
@@ -1,7 +1,7 @@
 /**
  * @file include/mpicxx/detail/utility.hpp
  * @author Marcel Breyer
- * @date 2020-04-19
+ * @date 2020-06-23
  *
  * @brief Defines utility functions used in the mpicxx library.
  */
@@ -9,40 +9,93 @@
 #ifndef MPICXX_UTILITY_HPP
 #define MPICXX_UTILITY_HPP
 
-#include <type_traits>
 #include <functional>
+#include <type_traits>
 
 
 namespace mpicxx::detail {
 
-    // TODO 2020-04-19 02:09 breyerml: comment, revise
+    /// @name utility functions checking if all values in a parameter pack fulfill a predicate
+    ///@{
+    /**
+     * @brief Checks whether invoking the predicate @p pred with @p t and @p u evaluates to `true`.
+     * @tparam BinaryOp a binary function (returning a `bool`).
+     * @tparam T an arbitrary type.
+     * @tparam U an arbitrary type.
+     * @param pred the binary function
+     * @param t first argument
+     * @param u second argument
+     * @return `true` if `pred(t, u)` evaluates to `true`, otherwise `false`
+     */
     template <typename BinaryOp, typename T, typename U>
     requires std::is_same_v<std::invoke_result_t<BinaryOp, T, U>, bool>
     bool all_same(BinaryOp pred, const T& t, const U& u) {
         return std::invoke(pred, t, u);
     }
-
+    /**
+     * @brief Checks whether the results of invoking the predicate @p pred with all parameters (@p t, @p u and @p args) compare equal.
+     * @details Evaluates: `pred(a, b) == pred(b, c) == ... == pred(y, z)`.
+     * @tparam BinaryOp a binary function (returning a `bool`).
+     * @tparam T an arbitrary type.
+     * @tparam U an arbitrary type.
+     * @tparam Args a parameter pack of arbitrary types.
+     * @param pred the binary function
+     * @param t first argument
+     * @param u second argument
+     * @param args remaining arguments
+     * @return `true` if all results compare equal, `false` otherwise
+     */
     template <typename BinaryOp, typename T, typename U, typename... Args>
     requires std::is_same_v<std::invoke_result_t<BinaryOp, T, U>, bool>
     bool all_same(BinaryOp pred, const T& t, const U& u, const Args&... args) {
         return std::invoke(pred, t, u) && all_same(pred, u, args...);
     }
 
-
+    /**
+     * @brief Checks whether the results of invoking the predicate @p pred with @p t and @p u compare equal.
+     * @tparam UnaryOp an unary function.
+     * @tparam T an arbitrary type.
+     * @tparam U an arbitrary type.
+     * @param pred the unary function
+     * @param t first argument
+     * @param u second argument
+     * @return `true` if `pred(t) == pred(u)`, otherwise `false`
+     */
     template <typename UnaryOp, typename T, typename U>
     bool all_same(UnaryOp pred, const T& t, const U& u) {
         return std::invoke(pred, t) == std::invoke(pred, u);
     }
-
+    /**
+     * @brief Checks whether the results of invoking the predicate @p pred with all parameters (@p t, @p u and @p args) compare equal.
+     * @tparam UnaryOp an unary function.
+     * @tparam T an arbitrary type.
+     * @tparam U an arbitrary type.
+     * @tparam Args a parameter pack of arbitrary types.
+     * @param pred the unary function
+     * @param t first argument
+     * @param u second argument
+     * @param args remaining arguments
+     * @return `true` if all results compare equal, otherwise `false`
+     */
     template <typename UnaryOp, typename T, typename U, typename... Args>
     bool all_same(UnaryOp pred, const T& t, const U& u, const Args&... args) {
         return (std::invoke(pred, t) == std::invoke(pred, u)) && all_same(pred, u, args...);
     }
 
-
+    /**
+     * @brief Checks whether all results of invoking @p pred compare equal.
+     *        Since only one parameter is given, this function returns always `true`.
+     * @tparam Op a function predicate.
+     * @tparam T the argument type.
+     * @param pred the function
+     * @param t the argument
+     * @return always `true`
+     */
     template <typename Op, typename T>
     bool all_same(Op pred, const T& t) { return true; }
+    ///@}
 
 }
+
 
 #endif //MPICXX_UTILITY_HPP
