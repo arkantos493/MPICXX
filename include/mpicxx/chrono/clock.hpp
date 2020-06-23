1b02aae1c86ed3b1e30878632954a47079037795
Marcel Breyer <b-marci@web.de>
2020-06-23 22:01:29 +0200

    documentation and formatting changes

--- include/mpicxx/chrono/clock.hpp
+++ include/mpicxx/chrono/clock.hpp
@@ -1,7 +1,7 @@
 /**
  * @file include/mpicxx/chrono/clock.hpp
  * @author Marcel Breyer
- * @date 2020-03-23
+ * @date 2020-06-23
  *
  * @brief Implements a wrapper class around the MPI timer functions.
  * @details These functions include *MPI_Wtime*, *MPI_Wtick* and the attribute *MPI_WTIME_IS_GLOBAL*.
@@ -25,60 +25,57 @@ namespace mpicxx {
     struct clock {
         /**
          * @brief Duration, a [`std::chrono::duration`](https://en.cppreference.com/w/cpp/chrono/duration) type used to measure the time
-         * since epoch.
+         *        since epoch.
          */
         using duration = std::chrono::duration<double>;
         /// An arithmetic type representing the number of ticks.
         using rep = duration::rep;
         /**
          * @brief `typename Period::type`, a [`std::ratio`](https://en.cppreference.com/w/cpp/numeric/ratio/ratio) representing the tick
-         * period (i.e. the number of seconds per tick).
+         *        period (i.e. the number of seconds per tick).
          */
         using period = duration::period;
         /// Represents a point in time associated with this custom clock.
         using time_point = std::chrono::time_point<clock>;
-        /// *MPI_Wtime* does **not** make any guarantees to be steady.
+        /// *MPI_Wtime* is steady.
         static const bool is_steady = true;
 
         /**
          * @brief Returns a floating-point number of seconds, representing elapsed wall-clock time since some time in the past.
-         * @return the elapsed wall-clock time
+         * @return the elapsed wall-clock time (`[[nodiscard]]`)
          *
-         * @calls{
-         * MPI_Wtime();      // exactly once
-         * }
+         * @calls{ MPI_Wtime();      // exactly once }
          */
-        [[nodiscard]] static time_point now() noexcept {
+        [[nodiscard]]
+        static time_point now() noexcept {
             return time_point(duration(MPI_Wtime()));
         }
 
         /**
          * @brief Returns the resolution of @ref mpicxx::clock::now() in seconds.
          * @details Example: if the clock is incremented every millisecond, this function would return \f$10^{-3}\f$.
-         * @return a double precision value (the number of seconds between successive clock ticks)
+         * @return the number of seconds between successive clock ticks (`[[nodiscard]]`)
          *
-         * @calls{
-         * double MPI_Wtick();      // exactly once
-         * }
+         * @calls{ double MPI_Wtick();      // exactly once }
          */
-        [[nodiscard]] static double resolution() noexcept {
+        [[nodiscard]]
+        static double resolution() noexcept {
             return MPI_Wtick();
         }
 
         // TODO 2020-02-18 19:39 marcel: change to the mpicxx equivalent
         /**
          * @brief Returns whether the clock is synchronized in the given communicator group (default: *MPI_COMM_WORLD*).
-         * @details The global variable *MPI_WTIME_IS_GLOBAL* is set to 1 if clocks at all processes in *MPI_COMM_WORLD* are synchronized,
-         * 0 otherwise. Because this variable need not be present when the clocks are not synchronized, the attribute key to
-         * *MPI_Comm_get_attr* is used, which is always valid.
+         * @details The global variable *MPI_WTIME_IS_GLOBAL* is set to `1` if clocks at all processes in *MPI_COMM_WORLD* are synchronized,
+         *          `0` otherwise. Because this variable need not be present when the clocks are not synchronized, the attribute key to
+         *          *MPI_Comm_get_attr* is used, which is always valid.
          * @param[in] comm the communicator for which the synchronization should be checked
-         * @return boolean variable that indicates whether clocks are synchronized
+         * @return `true` if the clocks are synchronized, otherwise `false`
          *
-         * @calls{
-         * int MPI_Comm_get_attr(MPI_Comm comm, int comm_keyval, void *attribute_val, int *flag);      // exactly once
-         * }
+         * @calls{ int MPI_Comm_get_attr(MPI_Comm comm, int comm_keyval, void *attribute_val, int *flag);      // exactly once }
          */
-        [[nodiscard]] static bool synchronized(MPI_Comm comm = MPI_COMM_WORLD) {
+        [[nodiscard]]
+        static bool synchronized(MPI_Comm comm = MPI_COMM_WORLD) {
             void* ptr;
             int flag;
             MPI_Comm_get_attr(comm, MPI_WTIME_IS_GLOBAL, &ptr, &flag);
@@ -91,5 +88,6 @@ namespace mpicxx {
     };
 
 }
+
 
 #endif // MPICXX_CLOCK_HPP
