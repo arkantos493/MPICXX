52ea6127113341fd280451d43bda3191d85eb4dd
Marcel Breyer <b-marci@web.de>
2020-07-24 22:44:16 +0200

    better file path

--- doc/html_style/append_to_file.cmake
+++ doc/html_style/append_to_file.cmake
@@ -1,5 +1,5 @@
-set(IN_FILE ${CMAKE_CURRENT_SOURCE_DIR}/../../doc/html_style/customsearch.js)
-set(OUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/../../doc/html/search/search.js)
+set(IN_FILE ${CMAKE_CURRENT_LIST_DIR}/customsearch.js)
+set(OUT_FILE ${CMAKE_CURRENT_LIST_DIR}/../html/search/search.js)
 
 file(READ ${IN_FILE} CONTENTS)
 file(APPEND ${OUT_FILE} "${CONTENTS}")
