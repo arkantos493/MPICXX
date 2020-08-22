aa096e26c8aa845d8acb3a6b167de9d3dd708886
Marcel Breyer <b-marci@web.de>
2020-08-22 21:24:02 +0200

    updated test

--- test/error/error_handler.cpp
+++ test/error/error_handler.cpp
@@ -1,7 +1,7 @@
 /**
  * @file
  * @author Marcel Breyer
- * @date 2020-08-19
+ * @date 2020-08-22
  * @copyright This file is distributed under the MIT License.
  *
  * @brief Test cases for the @ref mpicxx::error_handler class.
@@ -26,12 +26,14 @@ void exception_fn(const mpicxx::error_code ec) {
 
 TEST(ErrorHandlerTest, Tests) {
 
-    std::cout << static_cast<int>(mpicxx::error_handler::type::comm) << " " << mpicxx::error_handler::type::comm << std::endl;
-    std::cout << static_cast<int>(mpicxx::error_handler::type::file) << " " << mpicxx::error_handler::type::file << std::endl;
-    std::cout << static_cast<int>(mpicxx::error_handler::type::win)  << " " << mpicxx::error_handler::type::win  << std::endl;
+    std::cout << static_cast<int>(mpicxx::error_handler_type::comm) << " " << mpicxx::error_handler_type::comm << std::endl;
+    std::cout << static_cast<int>(mpicxx::error_handler_type::file) << " " << mpicxx::error_handler_type::file << std::endl;
+    std::cout << static_cast<int>(mpicxx::error_handler_type::win)  << " " << mpicxx::error_handler_type::win  << std::endl;
 
-    mpicxx::error_handler handler = mpicxx::make_error_handler<exception_fn>(mpicxx::error_handler::type::comm);
+    std::cout << "combined: " << (mpicxx::error_handler_type::comm | mpicxx::error_handler_type::file) << std::endl;
 
-    MPI_Comm_set_errhandler(MPI_COMM_WORLD, handler.handler_[0]);
-//    MPI_Comm_call_errhandler(MPI_COMM_WORLD, 42);
+    mpicxx::error_handler handler = mpicxx::make_error_handler<exception_fn>(mpicxx::error_handler_type::comm);
+
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, handler.get(mpicxx::error_handler_type::comm));
+    MPI_Comm_call_errhandler(MPI_COMM_WORLD, 42);
 }
