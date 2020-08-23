30afccd935b74c5b95cb5efdbf2673cc356b4af5
Marcel Breyer <b-marci@web.de>
2020-08-23 19:08:21 +0200

    changed concept to detect whether an enum or enum class implements the bitwise operators

--- include/mpicxx/detail/bitset.hpp
+++ include/mpicxx/detail/bitset.hpp
@@ -14,71 +14,83 @@
 #include <type_traits>
 
 namespace mpicxx::detail::bitset {
+    
+    template <typename T>
+    concept has_bitwise_operators = requires (T lhs, T rhs) {
+        { ~lhs }       -> std::convertible_to<typename std::remove_cvref_t<T>>;
+        { lhs | rhs }  -> std::convertible_to<typename std::remove_cvref_t<T>>;
+        { lhs & rhs }  -> std::convertible_to<typename std::remove_cvref_t<T>>;
+        { lhs ^ rhs }  -> std::convertible_to<typename std::remove_cvref_t<T>>;
+        { lhs |= rhs } -> std::convertible_to<typename std::remove_cvref_t<T>&>;
+        { lhs &= rhs } -> std::convertible_to<typename std::remove_cvref_t<T>&>;
+        { lhs ^= rhs } -> std::convertible_to<typename std::remove_cvref_t<T>&>;
+    };
 
     template <typename T>
-    concept is_enum = std::is_enum_v<T>;
+    concept is_bitwise_enum = std::is_enum_v<T> && has_bitwise_operators<T>;
+    
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr bool test(const T bitset, const T bit) noexcept {
         return (bitset & bit) == bit;
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr bool none(const T bitset) noexcept {
         return static_cast<std::underlying_type_t<T>>(bitset) == 0;
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr bool any(const T bitset) noexcept {
         return static_cast<std::underlying_type_t<T>>(bitset) != 0;
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr bool all(const T bitset) noexcept {
         return static_cast<std::underlying_type_t<T>>(bitset) == ~static_cast<std::underlying_type_t<std::remove_reference_t<T>>>(0);
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr int count(const T bitset) noexcept {
         return std::popcount(static_cast<std::underlying_type_t<T>>(bitset));
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr T set(const T) noexcept {
         return static_cast<T>(~static_cast<std::underlying_type_t<std::remove_reference_t<T>>>(0));
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr T set(const T bitset, const T bit) noexcept {
         return bitset | bit;
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr T reset(const T) noexcept {
         return static_cast<T>(0);
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr T reset(const T bitset, const T bit) noexcept {
         return bitset & (~bit);
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr T flip(const T bitset) noexcept {
         return ~bitset;
     }
 
-    template <is_enum T>
+    template <is_bitwise_enum T>
     [[nodiscard]]
     constexpr T flip(const T bitset, const T bit) noexcept {
         return bitset ^ bit;
